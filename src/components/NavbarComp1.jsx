1bf91724c06fe7317ef80f49443427869a8e8f0f
HANNIEL21 <dhanniel21@gmail.com>
2022-02-03 08:34:09 +0100

    first commit

--- src/components/NavbarComp1.jsx
+++ src/components/NavbarComp1.jsx
@@ -1,30 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-
-
-import { MDBBtn } from 'mdb-react-ui-kit';
-
 
 import {
     Navbar,
     Container,
-    Nav,
-    Form,
-    Badge
+    Nav
 } from 'react-bootstrap';
-import { FaUserAlt } from 'react-icons/fa';
-import { IoMdCart } from 'react-icons/io';
-
 const NavbarComp1 = () => {
 
-    const [basicModal, setBasicModal] = useState(false);
-
-    const toggleShow = () => setBasicModal(!basicModal);
-    const user = null;
-
     return (
-        <div className="position-sticky sticky-top">
-            <Navbar className="navbar p-0 h-25 sticky-top shadow t-0" bg="dark" expand="lg">
+        <div className="">
+            <Navbar className="navbar p-0 h-25" bg="transparent" expand="lg">
                 <Container className="container-fluid px-md-5">
                     <Navbar.Brand href="#" className='text-warning fs-1 fw-bold'>PEX</Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
@@ -34,26 +20,11 @@ const NavbarComp1 = () => {
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Link to="/meals" className="fw-bold text-decoration-none me-3 text-light">MEALS</Link>
-                            <Link to="/vendors" className="fw-bold text-decoration-none me-3 text-light">VENDORS</Link>
+                            <Link to="/" className="text-decoration-none me-3 text-black">About</Link>
+                            <Link to="/" className="text-decoration-none me-3 text-black">Vendors</Link>
+                            <Link to="/" className="text-decoration-none me-3 text-black">Terms</Link>
+                            <Link to="/" className="text-decoration-none me-3 text-black">PrivacyPolicy</Link>
                         </Nav>
-                        <Form className="d-flex">
-                            {user ? (
-                                <div className=''>
-                                    <div className="p-3 rounded-pill bg-warning">
-                                        {user.result.name.charAt[0]}
-                                    </div>
-                                </div>
-                            ) : (
-                                <Link to="/auth" variant="warning" className="btn bg-warning rounded-pill me-2"><FaUserAlt /></Link>
-                            )}
-                            <button type="button" className="btn btn-warning rounded-pill" data-bs-toggle="modal" data-bs-target="#exampleModal1">
-                                <IoMdCart /> <Badge bg="transparent" className="text-dark fw-bold">0</Badge>
-                                <span className="visually-hidden">unread messages</span>
-                            </button>
-                            
-
-                        </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,152 +1,122 @@
 import React from 'react';
-import {
-    Form,
-    FormControl,
-    Button
-} from 'react-bootstrap';
-import { HiOutlineSearch } from 'react-icons/hi';
-
-import { CardComp } from '../components/CardComp';
+import { Link } from 'react-router-dom';
 import NavbarComp1 from '../components/NavbarComp1';
 import FooterComp from '../components/FooterComp';
-
-import { cards } from '../components/list';
-import { Link } from 'react-router-dom';
 
 
 const HomePage = () => {
 
-    const card = cards.map((card, index) => {
-        return (
-            <CardComp
-                key={index}
-                {...card}
-            />
-        )
-    })
 
     return (
         <div className="h-100">
+            <NavbarComp1 />
             <main className='m-0'>
-                <NavbarComp1/>
-                <section className="mb-3  d-flex align-items-center justify-content-center home">
-                    <div className="overlay container-fluid d-flex align-items-center justify-content-center">
-                        <div className="row justify-content-center">
-                        <h1 className='display-1 fw-bold text-light w-75 text-center'>Your Favorite food, brought right to you.</h1>
-                            <div className="col-md-5 bg-white rounded-pill py-1 px-2 my-2 m-lg-50 w-sm-50 mt-5">
-                                <Form className="d-flex align-items-center">
-                                    <HiOutlineSearch className='fw-bold fs-1' />
-                                    <FormControl
-                                        type="search"
-                                        placeholder="Search for Meals, Drinks and Resturants"
-                                        className="border-0"
-                                        aria-label="Search"
-                                    />
-                                    <Button variant="warning rounded-pill px-3">Search</Button>
-                                </Form>
-                            </div>
-                    </div>
-                    </div>
-                </section>
+                <section className="py-5 d-flex flex-column align-items-center justify-content-center home">
+                    <div className="row g-4 container align-items-center">
+                        <div className="col-12 s col-md-6">
+                            <h1 className='display-4 fw-bold text-black'>Your favourite food brought
+                                right to you.</h1>
+                            <p className='text-black'>Download the PEX food app now on</p>
 
-                <section className='py-5'>
-                    <div className="container">
-                        <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill active" id="pills-popular-tab" data-bs-toggle="pill" data-bs-target="#pills-popular" type="button" role="tab" aria-controls="pills-popular" aria-selected="true">Popular</button>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill" id="pills-dessert-tab" data-bs-toggle="pill" data-bs-target="#pills-dessert" type="button" role="tab" aria-controls="pills-dessert" aria-selected="false">Dessert</button>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill" id="pills-chicken-tab" data-bs-toggle="pill" data-bs-target="#pills-chicken" type="button" role="tab" aria-controls="pills-chicken" aria-selected="false">Chicken</button>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill" id="pills-salad-tab" data-bs-toggle="pill" data-bs-target="#pills-salad" type="button" role="tab" aria-controls="pills-salad" aria-selected="false">Salads</button>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill" id="pills-drink-tab" data-bs-toggle="pill" data-bs-target="#pills-drink" type="button" role="tab" aria-controls="pills-drink" aria-selected="false">Drinks</button>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <button className="nav-link rounded-pill" id="pills-continential-tab" data-bs-toggle="pill" data-bs-target="#pills-continential" type="button" role="tab" aria-controls="pills-continential" aria-selected="false">Continential</button>
-                            </li>
-                        </ul>
-                        <div class="tab-content" id="pills-tabContent">
-                            <div className="tab-pane fade show active" id="pills-popular" role="tabpanel" aria-labelledby="pills-popular-tab">
-                                <div className="container">
-                                    <Link to data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                        <div className="row g-4">
-                                            {card}
-                                            {card}
-                                            {card}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="pills-dessert" role="tabpanel" aria-labelledby="pills-dessert-tab">
-                                <div className="container">
-                                    <Link to data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                        <div className="row g-4">
-                                            {card}
-                                            {card}
-                                            {card}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="pills-chicken" role="tabpanel" aria-labelledby="pills-chicken-tab">
-                                <div className="container">
-                                    <Link to data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                        <div className="row g-4">
-                                            {card}
-                                            {card}
-                                            {card}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="pills-salad" role="tabpanel" aria-labelledby="pills-salad-tab">
-                                <div className="container">
-                                    <Link to data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                        <div className="row g-4">
-                                            {card}
-                                            {card}
-                                            {card}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="pills-drink" role="tabpanel" aria-labelledby="pills-drink-tab">
-                                <div className="container">
-                                    <Link to data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                                        <div className="row g-4">
-                                            {card}
-                                            {card}
-                                            {card}
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="tab-pane fade" id="pills-continential" role="tabpanel" aria-labelledby="pills-continential-tab">
-                                <div className="container">
-                                    <div className="row g-4">
-                                        {card}
-                                        {card}
-                                        {card}
-                                    </div>
-                                </div>
-                            </div>
-                            
-
+                            <a href="https://github.com/" className="btn rounded-pill col-8 col-md-4 p-0"><img src="/image/playstore.svg" className='img-fluid' alt="" /></a>
+                        </div>
+                        <div className="col-md-6">
+                            <img src="/image/woman-phone.png" className='img-fluid w-100' alt="" />
                         </div>
                     </div>
                 </section>
 
-                <section className='bg-warning py-5'>
+                <section className='py-5 text-center'>
+                    <div className="container">
+                        <div className="row p-5">
+                            <h2>How the app works</h2>
+                            <p>Easily order your meal in 3 simple steps</p>
+                        </div>
+                    </div>
+                </section>
 
+                <section className='py-5 text-center sec2'>
+                    <div className="container">
+                        <div className="row text-start align-items-center justify-content-center">
+                            <div className="col-md-4">
+                                <img src="/image/Group2.png" className='img-fluid w-100' alt="" />
+                            </div>
+                            <div className="col-md-6 align-items-center">
+                                <h3 className="text-warning fw-bold m-0">Create an account</h3>
+                                <h2 className='fw-bold text-black'>Create/login to an existing account to get started</h2>
+                                <p className='lead fw-bold'>An account is created with your email and a desired password</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className='py-5 text-center sec3'>
+                    <div className="container">
+                        <div className="row text-start align-items-center justify-content-center">
+                            <div className="col-md-6 order-2">
+                                <h3 className="text-warning fw-bold m-0">Find a meal</h3>
+                                <h2 className='fw-bold text-black'>Shop for your favorites meal as e dey hot.</h2>
+                                <p className='lead fw-bold'>Shop for your favorites from your neighbourhood amala & ewedu to the shawarma and ice cream you crave.</p>
+                            </div>
+                            <div className="col-md-4 order-md-2 ">
+                                <img src="/image/Group1.png" className='img-fluid w-100' alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className='py-5 text-center '>
+                    <div className="container">
+                        <div className="row text-start align-items-center justify-content-center">
+                            <div className="col-md-4">
+                                <img src="/image/Group3.png" className='img-fluid w-100' alt="logo" />
+                            </div>
+                            <div className="col-md-6">
+                                <h3 className="text-warning fw-bold m-0">Checkout</h3>
+                                <h2 className='fw-bold text-black'>When you done, check outand get it delivered.</h2>
+                                <p className='lead fw-bold'>Just sit back and relax as we bring yourfavourite meals to your doorsteps.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section className='py-5 '>
+                    <div className="container-fluid">
+                        <div className="row justify-content-center">
+                            <div className="col-md-5">
+                                <img src="/image/image1.png" className='img-fluid w-100 m-0' alt="" />
+                            </div>
+                            <div className="col-md-6 bg-warning rounded-3 p-5">
+                                <div className="container">
+                                    <h2 className='fw-bold'>It’s all here, all in one app</h2>
+                                    <p className='lead'>
+                                        We have local teams dedicated to finding the best food in your city—whether that's
+                                        spaghett, rice, or a really satisfying shawarma—so you can get the coolest
+                                        restaurants brought right to you.
+                                    </p>
+                                    <Link to="/" className='btn bg-black rounded-pill px-4 text-warning btn-black fw-bold'>Get the app</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-2"></div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-2"></div>
+                            <div className="col-md-2"></div>
+                        </div>
+                    </div>
                 </section>
             </main>
-            <FooterComp/>
+            <footer>
+                <FooterComp />
+            </footer>
         </div>
     )
 }
